dd5397ef6be657526f14a37d5dfb790eae01e129
Dzokaredevil <dima.voytovich@mail.ru>
2018-05-06 17:29:19 +0300

    Add C++ management

---
+++ webdev.py
@@ -0,0 +1,97 @@
+from flask import Flask, render_template, request
+from runcode import runcode
+app = Flask(__name__)
+
+default_c_code = """#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    printf("Hello C World!!\\n");
+    return 0;
+}    
+"""
+
+default_cpp_code = """#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    cout << "Hello C++ World" << endl;
+    return 0;
+}
+"""
+
+default_py_code = """import sys
+import os
+
+if __name__ == "__main__":
+    print "Hello Python World!!"
+"""
+
+default_rows = "15"
+default_cols = "60"
+
+@app.route("/")
+@app.route("/runc", methods=['POST', 'GET'])
+def runc():
+    if request.method == 'POST':
+        code = request.form['code']
+        run = runcode.RunCCode(code)
+        rescompil, resrun = run.run_c_code()
+        if not resrun:
+            resrun = 'No result!'
+    else:
+        code = default_c_code
+        resrun = 'No result!'
+        rescompil = ''
+    return render_template("main.html",
+                           code=code,
+                           target="runc",
+                           resrun=resrun,
+                           rescomp=rescompil,
+                           rows=default_rows, cols=default_cols)
+
+@app.route("/cpp")
+@app.route("/runcpp", methods=['POST', 'GET'])
+def runcpp():
+    if request.method == 'POST':
+        code = request.form['code']
+        run = runcode.RunCppCode(code)
+        rescompil, resrun = run.run_cpp_code()
+        if not resrun:
+            resrun = 'No result!'
+    else:
+        code = default_cpp_code
+        resrun = 'No result!'
+        rescompil = ''
+    return render_template("main.html",
+                           code=code,
+                           target="runcpp",
+                           resrun=resrun,
+                           rescomp=rescompil,
+                           rows=default_rows, cols=default_cols)
+
+@app.route("/py")
+@app.route("/runpy", methods=['POST', 'GET'])
+def runpy():
+    if request.method == 'POST':
+        code = request.form['code']
+        run = runcode.RunPyCode(code)
+        rescompil, resrun = run.run_py_code()
+        if not resrun:
+            resrun = 'No result!'
+    else:
+        code = default_py_code
+        resrun = 'No result!'
+        rescompil = "No compilation for Python"
+        
+    return render_template("main.html",
+                           code=code,
+                           target="runpy",
+                           resrun=resrun,
+                           rescomp=rescompil,#"No compilation for Python",
+                           rows=default_rows, cols=default_cols)
+
+if __name__ == "__main__":
+    app.run()
